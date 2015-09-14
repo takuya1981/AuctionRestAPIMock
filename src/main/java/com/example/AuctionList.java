d1179fff061247709af32c85c9f6fb97780ee247
Takuya Fujiwara <mobilefujitaku@gmail.com>
2015-09-14 18:03:20 +0900

    AuctionList アノテーション追加

--- src/main/java/com/example/AuctionList.java
+++ src/main/java/com/example/AuctionList.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class AuctionList {
+	@XmlElement
 	public String id;
+	@XmlElement
 	public String name;
-
 	
 	public AuctionList(){}
 	
