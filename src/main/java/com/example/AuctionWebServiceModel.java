bfc0a206ab10a280bd980bf5a16c8e67768bd504
Takuya Fujiwara <mobilefujitaku@gmail.com>
2015-09-16 01:16:19 +0900

    postAuctionItem() model追加

--- src/main/java/com/example/AuctionWebServiceModel.java
+++ src/main/java/com/example/AuctionWebServiceModel.java
@@ -4,10 +4,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
+
 public class AuctionWebServiceModel {
 	// TODO 引数を受け取ってクエリを構築
 	public AuctionItemList getAuctionItemList()
 	{
+		// TODO token, userIDのチェック		
 		AuctionItemList list = new AuctionItemList(100, 10, 1);
 		try {
 			Connection con = getConnection();
@@ -25,6 +27,7 @@ public class AuctionWebServiceModel {
 	
 	public AuctionItemDetail getAuctionItemDetail(String auctionID)
 	{
+		// TODO token, userIDのチェック
 		AuctionItemDetail item = null;
 		try {
 			Connection con = getConnection();
@@ -33,7 +36,6 @@ public class AuctionWebServiceModel {
 			while(rs.next()){
 				item = new AuctionItemDetail(rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getInt(8), rs.getInt(9), rs.getInt(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14),rs.getBoolean(15), rs.getString(16), rs.getString(17), rs.getString(18));
 			}
-
 		} catch (URISyntaxException | SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
@@ -52,17 +54,15 @@ public class AuctionWebServiceModel {
 		return 0;
 	}
 	
-	public int updateBidder(String token, String userID, String auctionID, String bidPrice)
+	public int updateBidder(String userID, String user, String auctionID, String bidPrice)
 	{
+		// TODO token, userIDのチェック
+		// TODO 引数のデータをチェック
 		try {
-			// TODO token, userIDのチェック
-			// TODO 引数のデータをチェック
-			
-			// TODO auctionIDをkeyにbidPriceとhighestUserを更新する
+			// auctionIDをkeyにbidPriceとhighestUserを更新する
 			Connection con = getConnection();
-			
 	        Statement stmt = con.createStatement();
-	        stmt.executeUpdate("UPDATE");
+	        stmt.executeUpdate("UPDATE tbl SET highest_bidder_id = '" + userID + "', highest_bidder = '" + user + "', current_price = '" + bidPrice + "' where auction_id = '" + auctionID + "'");
 		} catch (URISyntaxException | SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
